d1b48a9f14722cbc7318cd90b060cf87a9ddcd1f
azamat62 <123293901+azamat62@users.noreply.github.com>
2023-02-19 22:18:44 +0600

    Add files via upload

---
+++ lab4/generator/2.py
@@ -0,0 +1,12 @@
+# N = int(input())
+# a = [i for i in range(0, N+1)]
+# b = [i for i in a if i%2 == 0]
+# print(b)
+
+#def ev_num(n):
+#     for i in range(0, n+1, 2):
+#         yield str(i)
+# n = int(input("Enter number: "))
+# ev_num = ev_num(n)
+# result = ','.join(ev_num)
+# print(result)
---
+++ lab4/math/2.py
@@ -0,0 +1,5 @@
+h = float(input())
+x = float(input())
+y = float(input())
+s = h*(x + y)/2
+print(s)
